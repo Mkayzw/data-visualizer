1a94e89f762d607d6bcffc658af9d48a7d843e63
Mkayzw <mkaytech1@gmail.com>
2025-01-02 12:21:10 +0200

    update package.json and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    }
+  },
+  server: {
+    host: true
+  }
 }) 
